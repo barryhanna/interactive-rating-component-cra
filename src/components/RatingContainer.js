3c92d90161a55d585df9cac7c069e7098a0666af
Barry Hanna <barryhanna@users.noreply.github.com>
2022-03-28 21:42:52 +0100

    Added markup and basic styling

--- src/components/RatingContainer.js
+++ src/components/RatingContainer.js
@@ -4,6 +4,7 @@ export default function RatingContainer() {
   return (
     <>
       <div class="rating-container">
+        <img src="/images/icon-star.svg" />
         <h3>How did we do?</h3>
         <p>
           Please let us know how we did with your support request. All feedback
@@ -11,11 +12,36 @@ export default function RatingContainer() {
         </p>
 
         <div class="rating-box">
-          <input type="radio" value="1" />
-          <input type="radio" value="2" />
-          <input type="radio" value="3" />
-          <input type="radio" value="4" />
-          <input type="radio" value="5" />
+          <div className="rating-item">
+            <label class="rating-number" for="one">
+              1
+            </label>
+            <input id="one" type="radio" value="1" name="rating" />
+          </div>
+          <div className="rating-item">
+            <label class="rating-number" for="two">
+              2
+            </label>
+            <input id="two" type="radio" value="2" name="rating" />
+          </div>
+          <div className="rating-item">
+            <label class="rating-number" for="three">
+              3
+            </label>
+            <input id="three" type="radio" value="3" name="rating" />
+          </div>
+          <div className="rating-item">
+            <label class="rating-number" for="four">
+              4
+            </label>
+            <input id="four" type="radio" value="4" name="rating" />
+          </div>
+          <div className="rating-item">
+            <label class="rating-number" for="five">
+              5
+            </label>
+            <input id="five" type="radio" value="5" name="rating" />
+          </div>
         </div>
 
         <button>Submit</button>
